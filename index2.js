01abb64456e6dd221d06a96bec9af74fc35a9c3c
NicoloMartiradonna <116425882+NicoloMartiradonna@users.noreply.github.com>
2022-10-29 00:08:26 +0200

    Add files via upload

---
+++ index2.js
@@ -0,0 +1,34 @@
+let w=900
+let h=450
+let c=0
+let delta=1
+
+
+
+
+function setup() {
+    createCanvas(w, h)
+}
+
+
+
+function draw() {
+    background(0, 255, 0)
+    noStroke()
+    
+
+   //c=c+delta
+   circle(c, h/2,60)
+   fill(255, 0, 0)    
+   circle(20, 180, 60)
+
+   fill(255, 0, 0)    
+   circle(20, 260, 60)
+    
+
+    
+
+
+  
+}
+
