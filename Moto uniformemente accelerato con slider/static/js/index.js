0601d292899898884e332abb2b0a723e028b2484
NicoloMartiradonna <martiradonna.nicolo@panettipitagora.edu.it>
2022-12-10 15:59:37 +0100

    Aggiunta accelerazione asse y

--- Moto uniformemente accelerato con slider/static/js/index.js
+++ Moto uniformemente accelerato con slider/static/js/index.js
@@ -1,15 +1,14 @@
-
-//mettere l'accelerazione sull'asse y
-//mettere l'input nel file html
-//fare due input ovvero uno sull'asse x e uno su quello y
-//mettere il quesryselctor per fare il collegamento "AXFIELD.addeventlistener"
+let w=400 //larghezza del canvas
+let h=400 //altezza del canvas
 let ax=-0.01 
 let vx=0
 let x=0
+let ay=-0.01 
+let vy=0
+let y=h/2
 let d=50
-let r=d/2
-let w=400 //larghezza del canvas
-let h=400 //altezza del canvas
+
+
 
 
 function setup() {
@@ -20,28 +19,55 @@ function setup() {
 function draw() {
   background(220);
   fill(255);
-  circle(x, 200, 50);
+  circle(x, y, d);
   vx=vx+ax;
   x=x+vx;
   
-  if(x>=425){
-    x=-25; 
+  if(x>=w+(d/2)){
+    x=-d/2; 
   }
   
-  if(x<-25){
-    x=425;
+  if(x<-d/2){
+    x=w+(d/2);
   }
+
+  vy=vy+ay;
+  y=y+vy;
+  
+  if(y>=h+(d/2)){
+    y=-d/2; 
+  }
+  
+  if(y<-d/2){
+    y=h+(d/2);
+  }
+
+
 }
 
 let axInput = document.querySelector("[name=ax]"),
-			vxInput = document.querySelector("[name=vx]")
+			vxInput = document.querySelector("[name=vx]"),
+      ayInput = document.querySelector("[name=ay]"),
+      vyInput = document.querySelector("[name=vy]")
     
-axInput.addEventListener("input", function(e){ 
+  axInput.addEventListener("input", function(e){ 
   ax = parseFloat(axInput.value)
   console.log(x + " " + vx + " " + ax)
 })
 
-vxInput.addEventListener("input", function(e){ 
+  vxInput.addEventListener("input", function(e){ 
   vx = parseFloat(vxInput.value)
   console.log(x + " " + vx + " " + ax)
 })
+
+ayInput.addEventListener("input", function(e){ 
+  ay = parseFloat(ayInput.value)
+  console.log(y + " " + vy + " " + ay)
+})
+
+  vyInput.addEventListener("input", function(e){ 
+  vy = parseFloat(vyInput.value)
+  console.log(y + " " + vy + " " + ay)
+})
+
+
