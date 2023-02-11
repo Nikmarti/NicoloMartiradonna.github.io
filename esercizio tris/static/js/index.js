50f9e2d9d0ab6efa459b71dd3894a2954e679987
NicoloMartiradonna <martiradonna.nicolo@panettipitagora.edu.it>
2023-02-11 16:30:27 +0100

    sistemato tris

--- esercizio tris/static/js/index.js
+++ esercizio tris/static/js/index.js
@@ -1,15 +1,25 @@
 let celleOccupate=0
+let fine=false
+
 
 let links = document.querySelectorAll(".gridBtn"),
 			currentPlayer = 1,
     cellIsOccupied = true
 
 	let td = document.querySelector(".risultato")
+	let d = document.querySelector(".pulsante")
 
-	let p=document.querySelector(".pulsante")
-
-
-	
+		d.addEventListener("click", function(e){
+			e.preventDefault()
+			td.innerHTML=``
+			for(let i=0; i<links.length; i++){
+				links[i].classList.remove("player1", "player2")
+			}
+			celleOccupate=0
+			currentPlayer = 1
+    		cellIsOccupied = true
+			fine=false
+		})
 
 		for(let i = 0; i < links.length; i++){
 			let link = links[i]
@@ -19,8 +29,9 @@ let links = document.querySelectorAll(".gridBtn"),
         
         console.log(cellIsOccupied)
 
-		if(!cellIsOccupied){
+		if(!cellIsOccupied && !fine){
 		celleOccupate++;
+		
 
         e.currentTarget.classList.toggle(`player${currentPlayer}`)//è LA STRINGA PLAYER E IL DOLALRO CONCATENA CON IL VALORE PLAYER ovvero player 1 e player 2
 		if((links[0].classList.contains(`player${currentPlayer}`) && links[1].classList.contains(`player${currentPlayer}`) && links[2].classList.contains(`player${currentPlayer}`))||
@@ -32,20 +43,25 @@ let links = document.querySelectorAll(".gridBtn"),
 		(links[0].classList.contains(`player${currentPlayer}`) && links[4].classList.contains(`player${currentPlayer}`) && links[8].classList.contains(`player${currentPlayer}`))||
 		(links[2].classList.contains(`player${currentPlayer}`) && links[4].classList.contains(`player${currentPlayer}`) && links[6].classList.contains(`player${currentPlayer}`))){
 			console.log(`player${currentPlayer} Ha vinto`)
+			if(currentPlayer==1){
+			td.style.color="blue"
+			}else{
+				td.style.color="red"
+			}
+
 			td.innerHTML=`player${currentPlayer} Ha vinto`
+			fine=true
 		}
 		else {
 			if(celleOccupate==9){
+				td.style.color="brown"
 				console.log("Nessuno ha vinto");
 				td.innerHTML=`Nessuno ha vinto`
+				fine=true
 			}
 		}
 
-
-		
-
-		
-		
+	
 	
 		currentPlayer = currentPlayer === 1 ? 2 : 1
         }
@@ -53,4 +69,3 @@ let links = document.querySelectorAll(".gridBtn"),
 		}
 
 		
-
